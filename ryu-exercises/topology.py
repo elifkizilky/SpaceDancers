5f3638473e4249086a87f20b752afd65cd7d4381
bgmrsln <begumarslan@outlook.de>
2023-11-14 14:57:15 +0300

    added new chnages related to switch config

---
+++ ryu-exercises/topology.py
@@ -0,0 +1,204 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+
+from ryu.topology import event, switches
+from ryu.topology.api import get_switch, get_link
+
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.topology_api_app = self
+
+
+    @set_ev_cls(event.EventSwitchEnter)
+    def get_topology_data(self, ev):
+        switch_list = get_switch(self.topology_api_app, None)
+        switches = [switch.dp.id for switch in switch_list]
+        links_list = get_link(self.topology_api_app, None)
+        links = [(link.src.dpid, link.dst.dpid, {'port': link.src.port_no}) for link in links_list]
+        print("switches ", switches)
+        print("links ", links)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        self.send_get_async_request(datapath)
+        self.send_set_async(datapath)
+        self.send_get_async_request(datapath)
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+    def send_set_async(self, datapath):
+        ofp = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+
+        packet_in_mask = 1 << ofp.OFPR_ACTION | 1 << ofp.OFPR_INVALID_TTL
+        port_status_mask = (1 << ofp.OFPPR_ADD
+                            | 1 << ofp.OFPPR_DELETE
+                            | 1 << ofp.OFPPR_MODIFY)
+        flow_removed_mask = (1 << ofp.OFPRR_IDLE_TIMEOUT
+                            | 1 << ofp.OFPRR_HARD_TIMEOUT
+                            | 1 << ofp.OFPRR_DELETE)
+        print(ofp.OFPRR_IDLE_TIMEOUT, ofp.OFPRR_HARD_TIMEOUT, ofp.OFPRR_DELETE)
+        req = ofp_parser.OFPSetAsync(datapath,
+                                    [packet_in_mask, 0],
+                                    [port_status_mask, 0],
+                                    [flow_removed_mask, 0])
+        datapath.send_msg(req)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    idle_timeout=1, hard_timeout=30, priority=priority, match=match,
+                                    instructions=inst, flags=ofproto.OFPFF_SEND_FLOW_REM)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    idle_timeout=1, hard_timeout=30, match=match, instructions=inst, flags= ofproto.OFPFF_SEND_FLOW_REM)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+    @set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
+    def flow_removed_handler(self, ev):
+        self.logger.info("debug-1")
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+
+        if msg.reason == ofp.OFPRR_IDLE_TIMEOUT:
+            reason = 'IDLE TIMEOUT'
+        elif msg.reason == ofp.OFPRR_HARD_TIMEOUT:
+            reason = 'HARD TIMEOUT'
+        elif msg.reason == ofp.OFPRR_DELETE:
+            reason = 'DELETE'
+        elif msg.reason == ofp.OFPRR_GROUP_DELETE:
+            reason = 'GROUP DELETE'
+        else:
+            self.logger.info("debug-2")
+            reason = 'unknown'
+        self.logger.info("debug-3")
+     
+        self.logger.info('OFPFlowRemoved received: '
+                        'cookie=%d priority=%d reason=%s table_id=%d '
+                        'duration_sec=%d duration_nsec=%d '
+                        'idle_timeout=%d hard_timeout=%d '
+                        'packet_count=%d byte_count=%d match.fields=%s',
+                        msg.cookie, msg.priority, reason, msg.table_id,
+                        msg.duration_sec, msg.duration_nsec,
+                        msg.idle_timeout, msg.hard_timeout,
+                        msg.packet_count, msg.byte_count, msg.match)
+
+    def send_get_async_request(self, datapath):
+        ofp_parser = datapath.ofproto_parser
+
+        req = ofp_parser.OFPGetAsyncRequest(datapath)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPGetAsyncReply, MAIN_DISPATCHER)
+    def get_async_reply_handler(self, ev):
+        msg = ev.msg
+
+        self.logger.info('OFPGetAsyncReply received: '
+                        'packet_in_mask=0x%08x:0x%08x '
+                        'port_status_mask=0x%08x:0x%08x '
+                        'flow_removed_mask=0x%08x:0x%08x',
+                        msg.packet_in_mask[0],
+                        msg.packet_in_mask[1],
+                        msg.port_status_mask[0],
+                        msg.port_status_mask[1],
+                        msg.flow_removed_mask[0],
+                        msg.flow_removed_mask[1])
