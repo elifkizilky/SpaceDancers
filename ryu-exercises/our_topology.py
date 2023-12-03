0832f2fd5e22d8c3b0040eacc885f12455f849e2
bgmrsln <begumarslan@outlook.de>
2023-12-03 18:25:55 +0300

    implemented topology by tcap file

--- ryu-exercises/our_topology.py
+++ ryu-exercises/our_topology.py
@@ -2,39 +2,88 @@ import subprocess
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.cli import CLI
-
+from mininet.node import RemoteController
+from scapy.all import *
+s1 =0
 class MyTopology(Topo):
     def build(self):
+        global s1
         # Add hosts
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
+        #h1 = self.addHost('h1')
+        #h2 = self.addHost('h2')
+        #h3 = self.addHost('h3')
 
         # Add switch
         s1 = self.addSwitch('s1')
 
         # Add links
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
+        #self.addLink(h1, s1)
+        #self.addLink(h2, s1)
+        #self.addLink(h3, s1)
+def create_hosts(topo, pcap_file):
+    global s1
+    hosts = {}
+    macs= {}
+    print("before rdpcap")
+    pkts = rdpcap(pcap_file)
+    print("after rdpcap")
+
+    
+    for pkt in pkts:
+        if pkt.haslayer(IP):
+            src_ip = pkt[IP].src
+            dst_ip = pkt[IP].dst
+            if pkt.haslayer(Ether):
+                src_mac = pkt[Ether].src
+                dst_mac = pkt[Ether].dst
+            # Check if source host exists, add if not
+            if src_ip not in hosts:
+                src_host = topo.addHost('h_' + src_ip.replace('.', '_'))
+                #src_host.setIP(src_ip)
+                print(src_host)
+                hosts[src_ip] = src_host
+                macs[src_host]= src_mac
+                topo.addLink(s1,src_host)
+                print("added: ", src_ip)
+
+            # Check if destination host exists, add if not
+            if dst_ip not in hosts:
+                dst_host = topo.addHost('h_' + dst_ip.replace('.', '_'))
+                hosts[dst_ip] = dst_host
+                macs[dst_host]= dst_mac
+                topo.addLink(s1,dst_host)
+                print("added dest: " , dst_ip)
+            print(topo.nodes())
+    print("hosts",topo.hosts())
+       
+    print("nodes",topo.nodes())
+     
+    return hosts, macs
+    
+  
 
 def start_mininet():
     topo = MyTopology()
-    net = Mininet(topo)
+    hosts, macs = create_hosts(topo, "my.pcap")
+    print("hosts inside start-mininet",hosts)
 
-    # Run Mininet with elevated privileges
-    subprocess.call(["sudo", "mn"])
+    for key, value in hosts.items():
+        print(f"{key}: {value}")
+    controller = RemoteController('c0', ip='127.0.0.1', port=6633)
+    net = Mininet(topo=topo, controller=controller)
 
     net.start()
+    for key, value in hosts.items():
+        host= net.getNodeByName(value)
+        print(host)
+        host.setIP(key)
+        host.setMAC(macs[value])
     
-    # Run your Ryu controller here
-    # For example:
-    net.controller = net.addController(name='ryu', controller=RemoteController, ip='127.0.0.1', port=6633)
-
-    # Launch Ryu controller (replace 'your_controller.py' with your actual Ryu controller script)
-    net.controller.cmd("ryu-manager ststRequest.py &")
-
     # Replay the pcap file into Mininet using tcpreplay
-    subprocess.call(["sudo", "tcpreplay", "-i", "eth0", "univ1_pt1"])
-
+    subprocess.call(["sudo", "tcpreplay", "-i","s1-eth1","--duration" ,"5","my.pcap"])
+    #subprocess.call(["sudo", "tcpreplay", "-i","s1-eth2","--duration" ,"5","my.pcap"])
+    #subprocess.call(["sudo", "tcpreplay", "-i","s1-eth3","--duration" ,"5","my.pcap"])
+    #subprocess.call(["sudo", "tcpreplay", "-i","s1-eth4","--duration" ,"5","my.pcap"])
     CLI(net)  # This drops you into a Mininet command prompt when the script is run
 
     net.stop()
